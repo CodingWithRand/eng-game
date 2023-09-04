933d773d5dab6c108f044a0c66187642f47eef31
thanwisitang7910@gmail.com <thanwisitang7910@gmail.com>
2023-09-04 12:54:02 +0700

    Select Lesson (2)

--- src/app/lessons/select-lesson/page.tsx
+++ src/app/lessons/select-lesson/page.tsx
@@ -11,6 +11,14 @@ export default function InitialPage() {
       <div className='head-text'>
         Select the lesson you'd like to learn
       </div>
+      <div className='lessons-list'>
+        <div className='lesson-choice'>
+          <button className='check-box'></button>
+          <label className='lesson-name'>
+            Premilary
+          </label>
+        </div>
+      </div>
     </main>
   );
 };
