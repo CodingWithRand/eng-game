3f43d06947701cb5d1fd435a36c7f6f7010d05f8
thanwisitang7910@gmail.com <thanwisitang7910@gmail.com>
2023-09-08 14:32:45 +0700

    Select Lesson (11.1.1)

--- src/app/lessons/page.tsx
+++ src/app/lessons/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
-import { MembershipStatus, MemberData } from '@/server/cookies';
+import { question, MembershipStatus, MemberData } from '@/server/cookies';
 
 const EmptyPage: React.FC = () => {
   const router = useRouter()
