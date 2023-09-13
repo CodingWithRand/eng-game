043d79dbdc1691d09539a41b5630ed59444a8625
thanwisitang7910@gmail.com <thanwisitang7910@gmail.com>
2023-09-13 11:32:29 +0700

    Update (23.3)

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -5,6 +5,7 @@ import { MemberData } from "@/server/cookies"
 export default function NavigationBar() {
     function SelectedLesson()
     {
+      if(MemberData.get("user") === undefined) return;
       if(MemberData.get("user").loggedIn !== null && MemberData.get("user").loggedIn === true && MemberData.get("user").lessons?.some((elem: string) => elem !== ''))
           return (
             <>
