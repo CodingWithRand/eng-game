41044b698499e013e5cfb3d9ef8a9c54a3c9ec1c
Rand0MTutoriaL <thanwisitang7910@gmail.com>
2023-09-14 23:22:02 +0700

    Update (27.1) rm firebase --temp

--- src/app/firebase-test/page.tsx
+++ src/app/firebase-test/page.tsx
@@ -1,21 +1,21 @@
-'use client'
+// 'use client'
 
-import { ref, set, onValue } from "firebase/database";
-import db from "@/firebase";
-import { useState } from "react";
+// import { ref, set, onValue } from "firebase/database";
+// import db from "@/firebase";
+// import { useState } from "react";
 
-export default function Test(){
-    const [ n, increament ] = useState(0)
-    const test = ref(db, 'test')
-    onValue(test, (snapshot) => {
-        const data = snapshot.val();
-        console.log('Data at path:', data);
-      });
-    const addItem = async () => {
-        increament(prevVal => prevVal + 1)
-        await set(test, n).then(() => console.log('pass')).catch((error) => console.log("it's error :(", error))
-    }
-    return(
-        <button onClick={addItem}>test</button>
-    )
-}
+// export default function Test(){
+//     const [ n, increament ] = useState(0)
+//     const test = ref(db, 'test')
+//     onValue(test, (snapshot) => {
+//         const data = snapshot.val();
+//         console.log('Data at path:', data);
+//       });
+//     const addItem = async () => {
+//         increament(prevVal => prevVal + 1)
+//         await set(test, n).then(() => console.log('pass')).catch((error) => console.log("it's error :(", error))
+//     }
+//     return(
+//         <button onClick={addItem}>test</button>
+//     )
+// }
