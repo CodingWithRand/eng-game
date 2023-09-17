285a4200301eeb46379ae5707f510e329d51e491
Rand0MTutoriaL <thanwisitang7910@gmail.com>
2023-09-17 23:17:28 +0700

    Update (33) firebase test (2)

--- src/app/firebase.ts
+++ src/app/firebase.ts
@@ -3,14 +3,14 @@ import { getDatabase } from 'firebase/database';
 import 'firebase/database';
 
 const clientCredentials = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
-    databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
+    apiKey: "AIzaSyDL8gOalvDEFuyfBAOCw1MMvYAcM5Io4dk",
+    authDomain: "tkate-game.firebaseapp.com",
+    databaseURL: "https://tkate-game-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "tkate-game",
+    storageBucket: "tkate-game.appspot.com",
+    messagingSenderId: "658411300836",
+    appId: "1:658411300836:web:a85bc58c6d7b92f5a249f1",
+    measurementId: "G-6X3BSCGWED"
 };
 
 
